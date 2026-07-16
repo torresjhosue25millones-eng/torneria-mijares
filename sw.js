efa725c44f9fe7a4de1bf95be9ead6296c20551c
torresjhosue25millones-eng <torresjhosue25millones@gmail.com>
2026-07-16 11:49:40 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'tm-v1';
+const SHELL = [
+  '/torneria-mijares/torneria_mijares_sync.html',
+  '/torneria-mijares/manifest.json',
+  '/torneria-mijares/icon-192.png',
+  '/torneria-mijares/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Para llamadas a la API de Google siempre ir a la red
+  if (e.request.url.includes('script.google.com')) return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
